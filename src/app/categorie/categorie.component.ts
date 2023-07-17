44e83747db9b53a14833dd5e636baab61bbc2bfb
barhoumieya <barhoumi.eya@esprit.tn>
2023-07-17 08:46:01 +0200

    update

--- src/app/categorie/categorie.component.ts
+++ src/app/categorie/categorie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategorieService } from 'src/app/services/categorie.service'
 
 @Component({
   selector: 'app-categorie',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorieComponent implements OnInit {
 
-  constructor() { }
+  categories : any[];
+
+  constructor(private categorieservice: CategorieService) { }
 
   ngOnInit(): void {
+    this.getCategoriesList();
+    
   }
 
+  getCategoriesList(): void {
+    this.categorieservice.getCategoriesList().subscribe(
+      (data) => {
+        this.categories = data ;
+      }, 
+      (error) => {
+        console.log('Une erreur est produite lors de la récupération des des catégories:', error);
+      }
+    );
+  }
+
+  addCategorie(categorie: any): void {
+    this.categorieservice.addCategorie(categorie).subscribe(
+      (data) => {
+        console.log('Nouvelle catégorie ajoutée avec succés:', data);
+        this.getCategoriesList();
+      },
+      (error)=>{
+        console.log('une erreur est produite lors de lajout de la catégorie :', error);
+      }
+      );
+    
+  }
+
+  updateCategorie(categorieId:  string , updateCategorie: any ): void {
+    this.categorieservice.updateCategorie(categorieId, updateCategorie).subscribe(
+      (data)=> {
+        console.log('Catégorie mise à jour avec succés :', data );
+      },
+      (error)=>{
+        console.log('une erreur est produite lors de la mise à jour de la catégorie :',error);
+      }
+
+    );
+  }
+
+deleteCategorie(categorieId:  string): void {
+  this.categorieservice.deleteCategorie(categorieId).subscribe(
+    (data)=> {
+      console.log('Categorie supprimer avec succés :', data);
+      this.getCategoriesList();
+    },
+    (error)=>{
+      console.log('Une erreur est produite lors de la suppression de la catégorie:', error );
+    }
+  );
+}
+  
 }
--- src/app/services/categorie.service.ts
+++ src/app/services/categorie.service.ts
@@ -1,17 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategorieService {
-  private apiUrl ='http://localhost:9090/categorie'; //Remplacez l'URL
+  private apiUrl: string = environment.apiUrl; //Remplacez l'URL
 
   constructor(private http: HttpClient) { }
   
-
-  getAllCategories(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+// récupérer la liste des catégories
+  getCategoriesList(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/categorie`);
   }
+
+
+  //ajouter une nouvelle catégorie 
+  addCategorie(categories: any): Observable<any> {
+    return this.http.post(`$${this.apiUrl}/categorie`,categories );
+  }
+
+
+  // mettre à jour une catégorie existante par ID
+  updateCategorie(categorieId: string , updateCategories : any):Observable<any>{
+    return this.http.put(`$${this.apiUrl}/categorie/${categorieId}`,updateCategories);
+  }
+  
+
+  // supprimer une catégorie
+deleteCategorie(categorieId: string):Observable<any>{
+  return this.http.delete(`$${this.apiUrl}/categorie/${categorieId}`);
+}
 }
