c1bf0e3f7eee2b980466e107aef2d7d048639d5a
barhoumieya <barhoumi.eya@esprit.tn>
2023-07-17 06:30:39 +0200

    update

--- src/app/services/categorie.service.ts
+++ src/app/services/categorie.service.ts
@@ -1,10 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategorieService {
+  private apiUrl ='http://localhost:9090/categorie'; //Remplacez l'URL
+
+  constructor(private http: HttpClient) { }
   
 
-  constructor() { }
+  getAllCategories(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
